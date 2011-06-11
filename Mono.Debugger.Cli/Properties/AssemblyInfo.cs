00c87bf0858f6ee8c46810f659f70080c62e3bf6
XTZGZoReX <xtzgzorex@gmail.com>
2011-06-11 22:33:17 +0200

    Fix information in AssemblyInfo.cs.

--- Mono.Debugger.Cli/Properties/AssemblyInfo.cs
+++ Mono.Debugger.Cli/Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Mono.Debugger.Cli")]
-[assembly: AssemblyProduct("Mono.Debugger.Cli")]
+[assembly: AssemblyProduct("sdb-cli")]
 [assembly: AssemblyDescription("Command line interface to Mono.Debugger.Soft")]
-[assembly: AssemblyCompany("Novell")]
+[assembly: AssemblyCompany("Xamarin")]
 [assembly: AssemblyCopyright("Copyright © Alex Rønne Petersen 2011")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
